336def6f97151bccd7ca568a81a8cfe7e4f0bc96
muhfayizsyamsuddin <muhfayiz14@gmail.com>
2026-08-20 00:28:48 +0800

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const cors = require('cors');
 const orderRouter = require('./routers/order');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
